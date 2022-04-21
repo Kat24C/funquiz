4e0054dfb9db6efda710b8738d676c91a83bc04b
Kat24C <kcsurvey47@gmail.com>
2022-04-21 11:08:56 +0000

    Add hide to Begin button and remove hide from the riddle questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
-function beginGame() {
+let restartButton = document.getElementById('btn-begin');
+let riddleQues = document.getElementById('riddles');
 
+restartButton.addEventListener('click', beginRiddles);
+
+function beginRiddles() {
+    restartButton.classList.add('hide');
+    riddleQues.classList.remove('hide');
 }
 
 function selectedRiddle() {
@@ -16,31 +22,31 @@ function correctScore() {
 
 const riddles = [{
     riddle: "What starts with an e and ends with an e but only has one letter in it?", 
-    answer: [{a: "an elephant", correct: false}, 
-    {b: "an envelope", correct: true}, 
-    {c: "an ewe", correct: false}, 
-    {d: "an eye", correct: false}
+    answer: [{choice: "an elephant", correct: false}, 
+    {choice: "an envelope", correct: true}, 
+    {choice: "an ewe", correct: false}, 
+    {choice: "an eye", correct: false}
 ]
 }, {
     riddle: "What has to be broken before you can use it?", 
-    answer: [ {a: "a package", correct: false}, 
-    {b: "an envelope", correct: false}, 
-    {c: "a box", correct: false}, 
-    {d: "an egg", correct: true} 
+    answer: [ {choice: "a package", correct: false}, 
+    {choice: "an envelope", correct: false}, 
+    {choice: "a box", correct: false}, 
+    {choice: "an egg", correct: true} 
 ]
 }, {
     riddle: "What question can you never answer yes to?", 
-    answer: [{a: "Are you asleep yet?", correct: true}, 
-    {b: "Do you want a chocolate?", correct: false}, 
-    {c: "Are you happy?", correct: false}, 
-    {d: "Do you know the answer?", correct: false}
+    answer: [{choice: "Are you asleep yet?", correct: true}, 
+    {choice: "Do you want a chocolate?", correct: false}, 
+    {choice: "Are you happy?", correct: false}, 
+    {choice: "Do you know the answer?", correct: false}
 ]
 }, {
     riddle: "What goes up but never comes down?",  
-    answer: [{a: "Your temperature", correct: false}, 
-    {b: "The sun", correct: false}, 
-    {c: "The time", correct: false}, 
-    {d: "Your age", correct: true},
+    answer: [{choice: "Your temperature", correct: false}, 
+    {choice: "The sun", correct: false}, 
+    {choice: "The time", correct: false}, 
+    {choice: "Your age", correct: true},
     ]
 } 
 ]
