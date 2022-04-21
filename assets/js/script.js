7e542ae3a18a92994f3ffda40023255b655c4871
Kat24C <kcsurvey47@gmail.com>
2022-04-21 09:41:53 +0000

    Add riddles to javaScript with answers, add function names

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+function beginGame() {
+
+}
+
+function selectedRiddle() {
+
+}
+
+function nextRiddle() {
+
+}
+
+function correctScore() { 
+
+}
+
+const riddles = [{
+    riddle: "What starts with an e and ends with an e but only has one letter in it?", 
+    answer: [{a: "an elephant", correct: false}, 
+    {b: "an envelope", correct: true}, 
+    {c: "an ewe", correct: false}, 
+    {d: "an eye", correct: false}
+]
+}, {
+    riddle: "What has to be broken before you can use it?", 
+    answer: [ {a: "a package", correct: false}, 
+    {b: "an envelope", correct: false}, 
+    {c: "a box", correct: false}, 
+    {d: "an egg", correct: true} 
+]
+}, {
+    riddle: "What question can you never answer yes to?", 
+    answer: [{a: "Are you asleep yet?", correct: true}, 
+    {b: "Do you want a chocolate?", correct: false}, 
+    {c: "Are you happy?", correct: false}, 
+    {d: "Do you know the answer?", correct: false}
+]
+}, {
+    riddle: "What goes up but never comes down?",  
+    answer: [{a: "Your temperature", correct: false}, 
+    {b: "The sun", correct: false}, 
+    {c: "The time", correct: false}, 
+    {d: "Your age", correct: true},
+    ]
+} 
+]
